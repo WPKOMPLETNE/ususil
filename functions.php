c4615aba49248596eec173c55e1adb0c2aa890a9
Jakub <knytl.jakub@gmail.com>
2020-05-29 10:05:14 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+add_filter( 'generate_typography_default_fonts', function( $fonts ) {
+    $fonts[] = 'Play';
+
+    return $fonts;
+} );
+// END ENQUEUE PARENT ACTION
+add_action( 'widgets_init', 'wpk_widgets_init' );
+function wpk_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Lišta 1', 'ususil' ),
+        'id'            => 'lista-1',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title'  => '<h3 class="lista-title">',
+        'after_title'   => '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'          => __( 'Lišta 2', 'ususil' ),
+        'id'            => 'lista-2',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title'  => '<h3 class="lista-title">',
+        'after_title'   => '</h3>',
+    ) );
+	
+	register_sidebar( array(
+        'name'          => __( 'Lišta 3', 'ususil' ),
+        'id'            => 'lista-3',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title'  => '<h3 class="lista-title">',
+        'after_title'   => '</h3>',
+    ) );
+}
